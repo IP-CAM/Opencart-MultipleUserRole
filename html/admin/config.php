6ff8cbfca5f6ded46187e7c23e429c0adb3f9a0a
Jason H <lasthyun822@gmail.com>
2019-09-06 04:24:47 -0500

    New

---
+++ html/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://concedgegraphics.loc/admin/');
+define('HTTP_CATALOG', 'http://concedgegraphics.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://concedgegraphics.loc/admin/');
+define('HTTPS_CATALOG', 'http://concedgegraphics.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Develop/www/concedgegraphics.loc/html/admin/');
+define('DIR_SYSTEM', 'D:/Develop/www/concedgegraphics.loc/html/system/');
+define('DIR_IMAGE', 'D:/Develop/www/concedgegraphics.loc/html/image/');
+define('DIR_STORAGE', 'D:/Develop/www/concedgegraphics.loc/storage/');
+define('DIR_CATALOG', 'D:/Develop/www/concedgegraphics.loc/html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'concedge_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ html/catalog/language/ar/extension/module/newsletter.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']     = 'Newsletter';
+$_['newsletter_description']     = 'You may unsubscribe at any moment. For that purpose, please find our contact info.';
+$_['text_email']     = 'Email';
+$_['text_placeholder']   = 'Enter Your Email Address';
+$_['text_subscribe'] = 'OK';
+
+// Email
+$_['email_subject']       = 'Welcome';
+$_['email_content']       = 'Thank you for Subscription.';
+
+
+
+
